3605ef53e20620bf33ef3eba0a75931eb043a797
jpberse <jp.bersezio@gmail.com>
2023-12-13 20:04:37 -0300

    Fakestore API and print cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,14 @@
-function Card() {
+function Card({ price, title, images, category: {name}}) {
     return (
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
             <figure className='relative mb-2 w-full h-4/5'>
-                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-2 py-0.2'>Electronics</span>
-                <img className='w-full h-full object-cover rounded-lg' src='https://images.pexels.com/photos/277390/pexels-photo-277390.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='Watch'/>
+                <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-2 py-0.2'>{name}</span>
+                <img className='w-full h-full object-contain rounded-lg' src={images[0]} alt={title} />
                 <button className='absolute top-0 right-0 flex justify-center items-center bg-white/95 w-5 h-5 rounded-full m-2 p-3 font-medium'>+</button>
             </figure>
             <p className='flex justify-between'>
-                <span className='text-sm font-light'>Watch</span>
-                <span className='text-md font-medium'>$3000</span>
+                <span className='text-sm font-light truncate'>{title}</span>
+                <span className='text-md font-medium'>{`$${price}`}</span>
             </p>
         </div>
     )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react";
 import { Card } from "../../Components/Card";
+import { apiUrl } from "../../api";
 
 function Home() {
+    const [products, setProducts] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${apiUrl}/products`)
+                const data = await response.json()
+                setProducts(data);
+            } catch (error) {
+                console.error(`ups, ocurrio un error ${error}`);
+            }
+        }
+        fetchData()
+    }, [])
+
     return(
         <div>
             Home
-            <Card />
+            <section className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+            {
+                products?.map((product) => (
+                <Card key={product.id} {...product}/>
+                    ))
+            }
+            </section>
         </div>
     )
 }
